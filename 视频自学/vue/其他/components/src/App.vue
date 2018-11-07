75e7fe0201a7af18750a9a094de9d12a56a7dcb9
Zhang <Xunying.Zhang@spirent.com>
2018-11-07 17:43:09 +0800

    <feat>(vue/componnet) add header component

--- 视频自学/vue/其他/components/src/App.vue
+++ 视频自学/vue/其他/components/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-      
+     <app-header></app-header> 
+     <app-login></app-login>
   </div>
 </template>
 
 <script>
+import appHeader from './component/header.vue';
+import appLogin from "./component/login.vue";
 export default {
-
-  
+	components:{
+		appHeader,
+		appLogin
+	}  
 }
 </script>
 
 <style lang="less" rel="stylesheet/less">
-
+body{
+	padding: 0;
+	margin: 0;
+}
 </style>
